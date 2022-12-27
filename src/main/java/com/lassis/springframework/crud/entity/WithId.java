949289136d66427a315b1cdd72e6480f388fcbe5
lassis <leassis@yahoo.com.br>
2022-12-27 19:03:19 +0100

    WithId allow return null

--- src/main/java/com/lassis/springframework/crud/entity/WithId.java
+++ src/main/java/com/lassis/springframework/crud/entity/WithId.java
@@ -1,10 +1,7 @@
 package com.lassis.springframework.crud.entity;
 
-import org.springframework.lang.NonNull;
-
 import java.io.Serializable;
 
 public interface WithId<ID extends Serializable> extends Serializable {
-    @NonNull
     ID getId();
 }
